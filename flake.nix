f6ba179e153b4d0aa1d93fe08bb4b9fcb1b95760
justinChen <justin.chen@thinktrace.com>
2025-12-11 18:21:28 +0800

    remove unused var

--- flake.nix
+++ flake.nix
@@ -1,37 +1,38 @@
 {
-  description =
-    "A collection of flake templates as starting points for your awesome projects";
+  description = "A collection of flake templates as starting points for your awesome projects";
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    templates = {
-      cpp = {
-        path = ./templates/cpp;
-        description = "Sy standard c++ env.";
-      };
-      nodejs-yarn = {
-        path = ./templates/nodejs-yarn;
-        description = "yarn nodejs";
-      };
-      nodejs = {
-        path = ./templates/nodejs;
-        description = "npm nodejs";
-      };
-      python-uv = {
-        path = ./templates/python-uv;
-        description = "Standard python env (using uv)";
-      };
-      python = {
-        path = ./templates/python;
-        description = "Standard python env.";
-      };
-      rust = {
-        path = ./templates/rust;
-        description = "Standard rust env.";
-      };
-      haskell = {
-        path = ./templates/haskell;
-        description = "Sy standard haskell env.";
+  outputs =
+    { }:
+    {
+      templates = {
+        cpp = {
+          path = ./templates/cpp;
+          description = "Sy standard c++ env.";
+        };
+        nodejs-yarn = {
+          path = ./templates/nodejs-yarn;
+          description = "yarn nodejs";
+        };
+        nodejs = {
+          path = ./templates/nodejs;
+          description = "npm nodejs";
+        };
+        python-uv = {
+          path = ./templates/python-uv;
+          description = "Standard python env (using uv)";
+        };
+        python = {
+          path = ./templates/python;
+          description = "Standard python env.";
+        };
+        rust = {
+          path = ./templates/rust;
+          description = "Standard rust env.";
+        };
+        haskell = {
+          path = ./templates/haskell;
+          description = "Sy standard haskell env.";
+        };
       };
     };
-  };
 }
